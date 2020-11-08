eebc4e4b9312f6d70ac136c94e4bd47a8f7d92cb
matthewbetts <matt.betts98@gmail.com>
2020-11-08 12:54:22 -0500

    Revert "wangobango"

--- infinite_P.m
+++
@@ -1,9 +0,0 @@
-function [ P ] = infinite_P(sigma,J,Kv,Ko,Km,Ft)
-%Infinite P finds the Pitch of a gear interface required in order to hae
-%the bending stress<infinite life endurance limit when al other paramters
-%are known.
-%subbed in 14/P for b
-P=sqrt((sigma*14*J)/(Ft*Kv*Ko*0.93*Km));
-
-end
-
