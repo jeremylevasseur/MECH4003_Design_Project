3d391abc6e044c09fb49b25908dd0f0869f7e0b6
jamescalexander <jamescalexander@cmail.carleton.ca>
2020-11-08 12:35:43 -0500

    wangobango

--- find_sigma.m
+++ find_sigma.m
@@ -3,6 +3,6 @@ function[sigma]= find_sigma(Ft,P,Kv,Ko,Km,b,J);
 
 %Find sigma calculates the bending stress in gear teeth
 
-sigma= (Ft*P*Kv*Ko*0.93*Km)/(b*J)
+sigma= (Ft*P*Kv*Ko*0.93*Km)/(b*J);
 end
 
