07492bbe57bfd8d33a62541395759a3f4a75f015
zachriley91 <73957789+zachriley91@users.noreply.github.com>
2020-11-10 17:14:57 -0500

    Update ShaftTorque.m

--- ShaftTorque.m
+++ ShaftTorque.m
@@ -1,12 +1,12 @@
-function[Torque] = ShaftTorque(P,RPM,SF)
+function[Torque] = ShaftTorque(Power,RPM,SF)
 %SHAFTTORQUE finds the torque in a given shaft
-%   Inputs: P is power in Watts
+%   Inputs: Power is power in Watts
 %           RPM is the rotations per minute of the shaft
 %           SF is the safety factor
 %   Outputs:    Torque is the torque applied to the shaft in Newton Metres
 
 %torque calculation
-Torque = P*SF/(2*pi()*RPM*(1/60)); %N.m
+Torque = (Power*SF)/(2*pi()*RPM*(1/60)); %N.m
 
 %outputs torque
 end
